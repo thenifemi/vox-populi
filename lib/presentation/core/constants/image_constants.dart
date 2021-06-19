094f12ef415cd9798897c9843c56fa8b07319399
Nifemi <thenifemi@gmail.com>
2021-06-19 16:06:41 -0300

    Vox Text Logo

--- lib/presentation/core/constants/image_constants.dart
+++ lib/presentation/core/constants/image_constants.dart
@@ -3,3 +3,5 @@ const String voxLogoBlack = "assets/logos/vox_logo_black.png";
 const String voxLogoWhite = "assets/logos/vox_logo_white.png";
 const String voxIconLogoBlack = "assets/logos/vox_icon_logo_black.png";
 const String voxIconLogoWhite = "assets/logos/vox_icon_logo_white.png";
+const String voxTextLogoBlack = "assets/logos/vox_text_logo_black.png";
+const String voxTextLogoWhite = "assets/logos/vox_text_logo_white.png";
