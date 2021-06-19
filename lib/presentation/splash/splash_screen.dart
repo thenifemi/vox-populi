8c7213aaa6d2632ab865a5375712ca84e591becc
Nifemi <thenifemi@gmail.com>
2021-06-19 15:36:46 -0300

    Splash Screen Animated Logo

--- lib/presentation/splash/splash_screen.dart
+++ lib/presentation/splash/splash_screen.dart
@@ -1,14 +1,44 @@
-import 'package:flutter/material.dart';
+import 'dart:async';
 
-class SplashScreen extends StatelessWidget {
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
+
+import '../core/constants/image_constants.dart';
+
+class SplashScreen extends HookWidget {
   const SplashScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(
-        child: Container(),
-      ),
-    );
+    final heightSize = MediaQuery.of(context).size.height;
+
+    final imageSize = useState(heightSize);
+
+    Future rebuidAfter1Second() async {
+      Timer(const Duration(seconds: 1), () {
+        imageSize.value = 200;
+      });
+    }
+
+    return FutureBuilder<dynamic>(
+        future: rebuidAfter1Second(),
+        builder: (context, snapshot) {
+          return AnnotatedRegion<SystemUiOverlayStyle>(
+            value: const SystemUiOverlayStyle(
+              statusBarColor: Colors.transparent,
+              statusBarIconBrightness: Brightness.dark,
+            ),
+            child: Scaffold(
+              body: Center(
+                child: AnimatedContainer(
+                  height: imageSize.value,
+                  duration: const Duration(milliseconds: 800),
+                  child: Image.asset(voxIconLogoBlack),
+                ),
+              ),
+            ),
+          );
+        });
   }
 }
