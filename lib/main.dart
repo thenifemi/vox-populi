3f1b55ed69a294849c7c6bd9185afc3f1c467a5a
Nifemi <thenifemi@gmail.com>
2021-06-19 14:23:20 -0300

    Setting up - Launcher Icon and Native Splash

--- lib/main.dart
+++ lib/main.dart
@@ -6,7 +6,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Material App',
+      title: 'Vox Populi',
       home: Scaffold(
         appBar: AppBar(
           title: Text('Material App Bar'),
