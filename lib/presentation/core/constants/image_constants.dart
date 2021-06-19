1bc6d25138bd7fed197f93c5c0c0e5543663e53b
Nifemi <thenifemi@gmail.com>
2021-06-19 15:05:11 -0300

    Setting up - Image Constants

---
+++ lib/presentation/core/constants/image_constants.dart
@@ -0,0 +1,5 @@
+//? Logos
+const String voxLogoBlack = "assets/logos/vox_logo_black.png";
+const String voxLogoWhite = "assets/logos/vox_logo_white.png";
+const String voxIconLogoBlack = "assets/logos/vox_icon_logo_black.png";
+const String voxIconLogoWhite = "assets/logos/vox_icon_logo_white.png";
