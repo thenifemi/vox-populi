9326f280d0e3bf9eb579907d617a73d607553d82
Nifemi <thenifemi@gmail.com>
2021-06-20 15:45:58 -0300

    Fade in transition

--- lib/presentation/get_started/get_started_screen.dart
+++ lib/presentation/get_started/get_started_screen.dart
@@ -1,14 +1,39 @@
+import 'package:animations/animations.dart';
 import 'package:flutter/material.dart';
 
 import '../core/constants/image_constants.dart';
 
-class GetStartedScreen extends StatelessWidget {
+class GetStartedScreen extends StatefulWidget {
   const GetStartedScreen({Key? key}) : super(key: key);
+
+  @override
+  _GetStartedScreenState createState() => _GetStartedScreenState();
+}
+
+class _GetStartedScreenState extends State<GetStartedScreen>
+    with SingleTickerProviderStateMixin {
+  AnimationController? _controller;
+
+  @override
+  void initState() {
+    _controller = AnimationController(
+      value: 0,
+      vsync: this,
+      duration: const Duration(seconds: 1),
+      reverseDuration: const Duration(seconds: 2),
+    );
+
+    _controller?.forward();
+
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
     final heightSize = MediaQuery.of(context).size.height;
     final widthSize = MediaQuery.of(context).size.width;
+
+    _controller?.forward();
 
     return Scaffold(
       body: SizedBox(
@@ -37,30 +62,40 @@ class GetStartedScreen extends StatelessWidget {
               ),
             ),
             Expanded(
-              child: Container(
-                decoration: const BoxDecoration(
-                  image: DecorationImage(
-                    image: AssetImage(
-                      getStartedBottomBg,
-                    ),
-                    fit: BoxFit.fill,
-                  ),
+              child: AnimatedBuilder(
+                animation: _controller!,
+                builder: (context, child) => FadeScaleTransition(
+                  animation: _controller!,
+                  child: child,
                 ),
-                child: Stack(
-                  children: [
-                    Center(
-                      child: Image.asset(
-                        voxLogoBlack,
-                        width: widthSize / 1.26,
+                child: Visibility(
+                  visible: _controller?.status != AnimationStatus.dismissed,
+                  child: Container(
+                    decoration: const BoxDecoration(
+                      image: DecorationImage(
+                        image: AssetImage(
+                          getStartedBottomBg,
+                        ),
+                        fit: BoxFit.fill,
                       ),
                     ),
-                    Center(
-                      child: Image.asset(
-                        voxLogoWhite,
-                        width: widthSize / 1.2,
-                      ),
+                    child: Stack(
+                      children: [
+                        Center(
+                          child: Image.asset(
+                            voxLogoBlack,
+                            width: widthSize / 1.26,
+                          ),
+                        ),
+                        Center(
+                          child: Image.asset(
+                            voxLogoWhite,
+                            width: widthSize / 1.2,
+                          ),
+                        ),
+                      ],
                     ),
-                  ],
+                  ),
                 ),
               ),
             ),
@@ -68,5 +103,11 @@ class GetStartedScreen extends StatelessWidget {
         ),
       ),
     );
+  }
+
+  @override
+  void dispose() {
+    _controller?.dispose();
+    super.dispose();
   }
 }
