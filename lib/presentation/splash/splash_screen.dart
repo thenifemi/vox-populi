05713d1b23e0fd412b5b9089acd3c48f461e04ea
Nifemi <thenifemi@gmail.com>
2021-06-20 02:33:20 -0300

    Animation Perfected

--- lib/presentation/splash/splash_screen.dart
+++ lib/presentation/splash/splash_screen.dart
@@ -3,38 +3,47 @@ import 'dart:async';
 import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
-import 'package:flutter_hooks/flutter_hooks.dart';
 
 import '../core/constants/image_constants.dart';
 import '../routes/router.gr.dart';
 
-class SplashScreen extends HookWidget {
+class SplashScreen extends StatefulWidget {
   const SplashScreen({Key? key}) : super(key: key);
 
-  Future<void> navToGetStarted(BuildContext context) async {
-    Timer(const Duration(seconds: 1), () {
-      context.router.replace(const GetStartedScreenRoute());
-    });
+  @override
+  _SplashScreenState createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  double? imageSize;
+
+  @override
+  void initState() {
+    imageSize = 400;
+    super.initState();
   }
 
-  Future rebuidAfter1Second(
-    BuildContext context,
-    ValueNotifier<double> imageSize,
-  ) async {
-    Timer(const Duration(seconds: 1), () async {
-      imageSize.value = 200.0;
-      await navToGetStarted(context);
-    });
+  Future<void> navToGetStarted() async {
+    context.router.replace(const GetStartedScreenRoute());
+  }
+
+  Future rebuidAfter1Second() async {
+    return Future.delayed(const Duration(seconds: 1), () {
+      setState(() {
+        imageSize = 200.0;
+      });
+    }).then((value) => Future.delayed(
+          const Duration(seconds: 1),
+          () {},
+        ));
   }
 
   @override
   Widget build(BuildContext context) {
-    final heightSize = MediaQuery.of(context).size.height;
-
-    final imageSize = useState(heightSize * 0.4);
-
     return FutureBuilder<dynamic>(
-        future: rebuidAfter1Second(context, imageSize),
+        future: rebuidAfter1Second().then(
+          (value) => context.router.replace(const GetStartedScreenRoute()),
+        ),
         builder: (context, snapshot) {
           return AnnotatedRegion<SystemUiOverlayStyle>(
             value: const SystemUiOverlayStyle(
@@ -46,7 +55,7 @@ class SplashScreen extends HookWidget {
                 child: Hero(
                   tag: 'logo',
                   child: AnimatedContainer(
-                    height: imageSize.value,
+                    height: imageSize,
                     duration: const Duration(milliseconds: 800),
                     child: Image.asset(voxIconLogoBlack),
                   ),
@@ -55,5 +64,10 @@ class SplashScreen extends HookWidget {
             ),
           );
         });
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
   }
 }
