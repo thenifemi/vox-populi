3fdd683a5352873b74c5f14427deda6549e51065
Nifemi <thenifemi@gmail.com>
2021-06-20 02:39:23 -0300

    App Colors

---
+++ lib/presentation/core/constants/color_constants.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const Color black = Color(0xFF020403);
+  static const Color white = Color(0xFFFFFFFF);
+  static const Color grey = Color(0xFF212529);
+  static const Color greyAccent = Color(0xFF495057);
+  static const Color eggshell = Color(0xFFe9ecef);
+}
