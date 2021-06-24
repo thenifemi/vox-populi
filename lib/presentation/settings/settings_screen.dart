ba3ce6fefa8a7e5ec84300626a7d8629df10f2cd
Nifemi <thenifemi@gmail.com>
2021-06-24 10:27:31 -0300

    Settings Screen 3 - Edit Profile

--- lib/presentation/settings/settings_screen.dart
+++ lib/presentation/settings/settings_screen.dart
@@ -2,10 +2,14 @@ import 'package:auto_route/auto_route.dart';
 import 'package:auto_size_text/auto_size_text.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:hive/hive.dart';
+import 'package:vox_populi/presentation/core/components/app_button.dart';
 
 import '../../Application/theme/theme_bloc.dart';
+import '../../Domain/user/user.dart';
 import '../core/components/app_annotated_widget.dart';
 import '../core/constants/color_constants.dart';
+import '../core/constants/image_constants.dart';
 
 class SettingsScreen extends StatelessWidget {
   const SettingsScreen({Key? key}) : super(key: key);
@@ -18,6 +22,8 @@ class SettingsScreen extends StatelessWidget {
     final theme = BlocProvider.of<ThemeBloc>(context).state.themeData;
     final appTheme = BlocProvider.of<ThemeBloc>(context).appTheme;
 
+    final userBox = Hive.box<User>('user');
+
     return AppAnnotatedWidget(
       appTheme: appTheme,
       child: Scaffold(
@@ -29,32 +35,51 @@ class SettingsScreen extends StatelessWidget {
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
               SizedBox(height: heightSize * 0.03),
-              Expanded(
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Row(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        AutoSizeText(
-                          "Settings",
-                          style: theme?.textTheme.headline4,
-                        ),
-                        const Spacer(),
-                        IconButton(
-                          onPressed: () {
-                            context.router.pop();
-                          },
-                          icon: Icon(
-                            Icons.cancel_rounded,
-                            size: 30,
-                            color: AppColors.eggshell.withOpacity(0.3),
-                          ),
-                        ),
-                      ],
+              Row(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  AutoSizeText(
+                    "Settings",
+                    style: theme?.textTheme.headline4,
+                  ),
+                  const Spacer(),
+                  IconButton(
+                    onPressed: () {
+                      context.router.pop();
+                    },
+                    icon: Icon(
+                      Icons.cancel_rounded,
+                      size: 30,
+                      color: AppColors.eggshell.withOpacity(0.3),
                     ),
-                  ],
-                ),
+                  ),
+                ],
+              ),
+              SizedBox(height: heightSize * 0.03),
+              Row(
+                children: [
+                  Hero(
+                    tag: 'avatar',
+                    child: Image.asset(
+                      man1,
+                      height: heightSize * 0.1,
+                    ),
+                  ),
+                  SizedBox(width: widthSize * 0.03),
+                  AutoSizeText(
+                    userBox.get(0)!.name!,
+                    style: theme?.textTheme.headline5,
+                  ),
+                  const Spacer(),
+                  SizedBox(
+                    height: heightSize * 0.03,
+                    child: AppButton(
+                      name: 'Edit',
+                      onPressed: () {},
+                      widthSize: null,
+                    ),
+                  ),
+                ],
               ),
             ],
           ),
