5ebbdfa1668fb97acb24cc2693ce919401f5e0e6
Nifemi <thenifemi@gmail.com>
2021-07-12 14:24:48 -0300

    Edit Profile  Screen 1

--- lib/presentation/edit_profile/edit_profile_screen.dart
+++ lib/presentation/edit_profile/edit_profile_screen.dart
@@ -1,20 +1,24 @@
+import 'package:auto_size_text/auto_size_text.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:vox_populi/presentation/core/components/app_button.dart';
 
 import '../../Application/theme/theme_bloc.dart';
 import '../../Domain/user/user.dart';
 import '../core/components/app_annotated_widget.dart';
+import '../core/components/app_button.dart';
 import '../core/constants/image_constants.dart';
+import 'widgets/edit_profile_textfield_widget.dart';
 import 'widgets/edit_profile_top_widget.dart';
 
 class EditProfileScreen extends StatelessWidget {
-  const EditProfileScreen({
+  EditProfileScreen({
     Key? key,
     required this.profile,
   }) : super(key: key);
 
   final User profile;
+
+  final formKey = GlobalKey<FormState>();
 
   @override
   Widget build(BuildContext context) {
@@ -27,39 +31,66 @@ class EditProfileScreen extends StatelessWidget {
     return AppAnnotatedWidget(
       appTheme: appTheme,
       child: Scaffold(
+        resizeToAvoidBottomInset: false,
         body: Container(
           height: heightSize,
           width: widthSize,
           padding: const EdgeInsets.all(30.0),
-          child: Column(
-            children: [
-              SizedBox(height: heightSize * 0.03),
-              EditProfileTopWidget(
-                theme: theme,
-                appTheme: appTheme,
-              ),
-              SizedBox(height: heightSize * 0.06),
-              Stack(
-                alignment: AlignmentDirectional.bottomCenter,
-                children: [
-                  Hero(
-                    tag: 'avatar',
-                    child: Image.asset(
-                      man1,
-                      height: heightSize * 0.18,
+          child: Form(
+            key: formKey,
+            child: Column(
+              children: [
+                SizedBox(height: heightSize * 0.03),
+                EditProfileTopWidget(
+                  theme: theme,
+                  appTheme: appTheme,
+                ),
+                SizedBox(height: heightSize * 0.06),
+                Stack(
+                  alignment: AlignmentDirectional.bottomCenter,
+                  children: [
+                    Hero(
+                      tag: 'avatar',
+                      child: Image.asset(
+                        man1,
+                        height: heightSize * 0.18,
+                      ),
                     ),
-                  ),
-                  SizedBox(
-                    height: heightSize * 0.03,
-                    child: AppButton(
-                      name: 'Edit',
-                      onPressed: () {},
-                      widthSize: null,
+                    SizedBox(
+                      height: heightSize * 0.03,
+                      child: AppButton(
+                        name: 'Edit',
+                        onPressed: () {},
+                        widthSize: null,
+                      ),
                     ),
-                  ),
-                ],
-              ),
-            ],
+                  ],
+                ),
+                SizedBox(height: heightSize * 0.03),
+                EditProfileTextfieldWidget(
+                  initialValue: profile.name,
+                  onSaved: (v) {},
+                  validator: (v) {},
+                ),
+                SizedBox(height: heightSize * 0.02),
+                AppButton(
+                  name: 'Save',
+                  onPressed: () {
+                    final form = formKey.currentState;
+
+                    if (form!.validate()) {
+                      form.save();
+                    }
+                  },
+                  widthSize: widthSize,
+                ),
+                const Spacer(),
+                AutoSizeText(
+                  "All your data is stored securely on your device, we do not collect any data.",
+                  style: theme?.textTheme.bodyText1,
+                ),
+              ],
+            ),
           ),
         ),
       ),
