0cddbf72ad1a108d74b50d12342845dea89c5833
Nifemi <thenifemi@gmail.com>
2021-07-13 14:38:47 -0300

    Domain - News Enums

---
+++ lib/Domain/news/enums.dart
@@ -0,0 +1,37 @@
+enum NewsCategory {
+  business,
+  entertainment,
+  general,
+  health,
+  science,
+  sports,
+  technology,
+}
+
+String newsCategoryToString(NewsCategory category) {
+  switch (category) {
+    case NewsCategory.business:
+      return "business";
+
+    case NewsCategory.entertainment:
+      return "entertainment";
+
+    case NewsCategory.general:
+      return "general";
+
+    case NewsCategory.health:
+      return "health";
+
+    case NewsCategory.science:
+      return "science";
+
+    case NewsCategory.sports:
+      return "sports";
+
+    case NewsCategory.technology:
+      return "technology";
+
+    default:
+      return "";
+  }
+}
