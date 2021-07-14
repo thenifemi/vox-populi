9fa8ad6bfee0e4acae94f9bb6f0921ef9193bae4
Nifemi <thenifemi@gmail.com>
2021-07-14 10:22:05 -0300

    Article in new file with Hive

---
+++ lib/Domain/news/article.dart
@@ -0,0 +1,79 @@
+import 'package:hive/hive.dart';
+
+part 'article.g.dart';
+
+@HiveType(typeId: 0)
+class Article {
+  Article({
+    this.source,
+    this.author,
+    this.title,
+    this.description,
+    this.url,
+    this.urlToImage,
+    this.publishedAt,
+    this.content,
+  });
+
+  @HiveField(0)
+  final Source? source;
+  @HiveField(1)
+  final String? author;
+  @HiveField(2)
+  final String? title;
+  @HiveField(3)
+  final String? description;
+  @HiveField(4)
+  final String? url;
+  @HiveField(5)
+  final String? urlToImage;
+  @HiveField(6)
+  final DateTime? publishedAt;
+  @HiveField(7)
+  final String? content;
+
+  factory Article.fromJson(Map<String, dynamic> json) => Article(
+        source: Source.fromJson(json["source"]),
+        author: json["author"],
+        title: json["title"],
+        description: json["description"],
+        url: json["url"],
+        urlToImage: json["urlToImage"],
+        publishedAt: DateTime.parse(json["publishedAt"]),
+        content: json["content"],
+      );
+
+  Map<String, dynamic> toJson() => {
+        "source": source?.toJson(),
+        "author": author,
+        "title": title,
+        "description": description,
+        "url": url,
+        "urlToImage": urlToImage,
+        "publishedAt": publishedAt?.toIso8601String(),
+        "content": content,
+      };
+}
+
+@HiveType(typeId: 1)
+class Source {
+  Source({
+    this.id,
+    this.name,
+  });
+
+  @HiveField(0)
+  final String? id;
+  @HiveField(1)
+  final String? name;
+
+  factory Source.fromJson(Map<String, dynamic> json) => Source(
+        id: json["id"],
+        name: json["name"],
+      );
+
+  Map<String, dynamic> toJson() => {
+        "id": id,
+        "name": name,
+      };
+}
--- lib/Domain/news/news_headlines_response.dart
+++ lib/Domain/news/news_headlines_response.dart
@@ -4,6 +4,8 @@
 
 import 'dart:convert';
 
+import 'article.dart';
+
 NewsHeadlinesResponse newsHeadlinesResponseFromJson(String str) =>
     NewsHeadlinesResponse.fromJson(json.decode(str));
 
@@ -33,69 +35,5 @@ class NewsHeadlinesResponse {
         "status": status,
         "totalResults": totalResults,
         "articles": List<dynamic>.from(articles!.map((x) => x?.toJson())),
-      };
-}
-
-class Article {
-  Article({
-    this.source,
-    this.author,
-    this.title,
-    this.description,
-    this.url,
-    this.urlToImage,
-    this.publishedAt,
-    this.content,
-  });
-
-  final Source? source;
-  final String? author;
-  final String? title;
-  final String? description;
-  final String? url;
-  final String? urlToImage;
-  final DateTime? publishedAt;
-  final String? content;
-
-  factory Article.fromJson(Map<String, dynamic> json) => Article(
-        source: Source.fromJson(json["source"]),
-        author: json["author"],
-        title: json["title"],
-        description: json["description"],
-        url: json["url"],
-        urlToImage: json["urlToImage"],
-        publishedAt: DateTime.parse(json["publishedAt"]),
-        content: json["content"],
-      );
-
-  Map<String, dynamic> toJson() => {
-        "source": source?.toJson(),
-        "author": author,
-        "title": title,
-        "description": description,
-        "url": url,
-        "urlToImage": urlToImage,
-        "publishedAt": publishedAt?.toIso8601String(),
-        "content": content,
-      };
-}
-
-class Source {
-  Source({
-    this.id,
-    this.name,
-  });
-
-  final String? id;
-  final String? name;
-
-  factory Source.fromJson(Map<String, dynamic> json) => Source(
-        id: json["id"],
-        name: json["name"],
-      );
-
-  Map<String, dynamic> toJson() => {
-        "id": id,
-        "name": name,
       };
 }
