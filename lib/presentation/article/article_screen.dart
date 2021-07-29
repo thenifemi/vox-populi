33c80120c3603c0376c2f1827382caebbd4cfb3f
Nifemi <thenifemi@gmail.com>
2021-07-29 16:03:06 -0300

    Article screen redesigned 2

--- lib/presentation/article/article_screen.dart
+++ lib/presentation/article/article_screen.dart
@@ -1,10 +1,7 @@
-import 'dart:ui';
-
 import 'package:auto_route/auto_route.dart';
 import 'package:auto_size_text/auto_size_text.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:flutter_svg/svg.dart';
 import 'package:intl/intl.dart';
 
 import '../../Application/theme/theme_bloc.dart';
